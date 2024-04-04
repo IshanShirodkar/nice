8f2bdee08edc1af68ab11a8058bd19b0e45f24b8
Ishan Shirodkar <ishanshirodkar@gmail.com>
2024-04-04 19:09:55 +0530

    Final..

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,5 +1,3 @@
-import { Prisma } from "@prisma/client";
-import { inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -60,8 +58,8 @@ export const profileRouter = createTRPCRouter({
         addedFollow = false;
       }
 
-      // void ctx.revalidateSSG?.(`/profiles/${userId}`);
-      // void ctx.revalidateSSG?.(`/profiles/${currentUserId}`);
+      void ctx.revalidateSSG?.(`/profiles/${userId}`);
+      void ctx.revalidateSSG?.(`/profiles/${currentUserId}`);
 
       return { addedFollow };
     }),
